60b5a4d053788c935b9ce1045837e813ee3cbec3
leticia710 <wtleealcantara@gmail.com>
2026-08-21 10:58:06 -0300

    Add student information and average calculations

--- aula02.py
+++ aula02.py
@@ -1,1 +1,40 @@
+print("hello,world")
+nome= "Letícia"
+idade= 18
+print ("Nome:", nome)
+print ("Idade:", idade)
+print("Bom dia!\nBem-vindo à aula.")
+print("Nome\tNota")
+print("Ana\t8")
+print("Pedro\t9")
+print("14", "08", "2026", sep="/")
+print("Olá", end=" ")
+print("Letícia")
+nome = "Letícia"
+idade = 18 
+curso = "Administração"
+nota = 10
+print("===== FICHA DO ALUNO =====")
+print("Nome:", nome)
+print("Idade:", idade)
+print("Curso:", curso)
+print("Nota:", nota)
+print("==========================")
+nome = "Letícia"
+nota1= 8
+nota2 = 10
+print("Aluno:", nome)
+print("Nota 1:", nota1)
+print("Nota 2:", nota2)
+print("Média:", (nota1 + nota2) / 2)
+nome= "Mariana"
+idade= 20
+curso= "Administração"
+nota1= 7.5
+nota2= 9
+print("Aluno:", nome)
+print("Nota 1:", nota1)
+print("Nota 2:", nota2)
+print("Média:", (nota1 + nota2) / 2)
+print(f" {nome} terminou a atividade com média", (nota1 + nota2) / 2)
 
