d9f63b7c7f09f034f3280aeb1f4e37a276611ffc
leticia710 <wtleealcantara@gmail.com>
2026-08-28 11:07:39 -0300

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,2 +1,33 @@
-print ("Hello,World")
-print ("Hi, my name is Letícia")
+nome= "Letícia"
+idade= 18
+nota= 8.5
+print(nome)
+print(idade)
+print(nota)
+pontos=10
+pontos= pontos+5
+print(pontos)
+numero1=10
+numero2=5
+soma=numero1+numero2
+print(soma)
+nota1=8
+nota2=7
+media=(nota1+nota2)/2
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}!")
+idade = input("Digite sua idade: ")
+print(type(idade))
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
+soma = numero1 + numero2
+print(f"Resultado: {soma}")
+nome = input("Nome: ")
+idade = int(input("Idade: "))
+nota1 = float(input("Nota 1: "))
+nota2 = float(input("Nota 2: "))
+media = (nota1 + nota2) / 2
+print("\n===== RESULTADO =====")
+print(f"Aluno: {nome}")
+print(f"Idade: {idade}")
+print(f"Média: {media}")
